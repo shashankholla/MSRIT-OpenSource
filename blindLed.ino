6ff793297c2af6bfbd0954aab9f2f4caac73c25d
shashankholla <shashu1999@gmail.com>
2018-10-06 18:30:21 +0530

    Add files via upload

---
+++ blindLed.ino
@@ -0,0 +1,47 @@
+#define LED1 10;
+#define LED1 11;
+#define LED1 12;
+#define LED1 13;
+
+
+int counter = 4;
+
+void setup() {
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+
+  for(counter = 4; counter > -1; counter--)
+  {
+    if(counter != 0)
+    {for(int i = 10; i < 14; i++)
+    {
+      digitalWrite(i,HIGH);
+      delay(500 );
+      digitalWrite(i,LOW);
+      delay(1000*counter);
+    }}
+    else{
+      for(int i = 10; i < 14; i++)
+    {
+      digitalWrite(i,HIGH);           
+    }
+    delay(1000);
+    for(int i = 10; i < 14; i++)
+    {
+      digitalWrite(i,LOW);           
+    }
+      
+    }
+  }
+  
+  
+  
+  
+
+}
